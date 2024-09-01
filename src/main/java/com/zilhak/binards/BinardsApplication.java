50fad7b5e348581ed3309b73b0ebe106aee99c58
이재학 <zilhak3@naver.com>
2024-09-01 20:34:39 +0900

    Initialize Spring boot

---
+++ src/main/java/com/zilhak/binards/BinardsApplication.java
@@ -0,0 +1,13 @@
+package com.zilhak.binards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinardsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zilhak/binards/BinardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zilhak.binards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
